57b77f0198d1c748138be288db2d3fbfd319a19c
Joanna Duda <duda.joann@gmail.com>
2021-01-12 00:44:33 +0100

    add some tests

--- day_6_image_resizer/day6.py
+++ day_6_image_resizer/day6.py
@@ -5,19 +5,37 @@ from typing import Dict
 
 class ImageResizer:
     def __init__(self, file_path: str, destination_path: str) -> None:
+        """
+        :param file_path: folder path were original images are saved,
+        :param destination_path:  folder path were processed images should be saved
+        """
         self.file_path = file_path
         self.destination_path = destination_path
 
     def read_all_in(self) -> Dict:
-        image_paths = {file: os.path.join(self.file_path, file) for file in os.listdir(self.file_path) if file.endswith('.jpg')}
+        """
+        get all file paths from folder directory
+        :return: dictionary with key: file name and value as file path
+        """
+        image_paths = {file: os.path.join(self.file_path, file)
+                       for file in os.listdir(self.file_path) if file.endswith('.jpg')}
         return image_paths
 
     @staticmethod
     def get_pixels_size_of(image: Image) -> tuple:
+        """
+        get pixels size of image
+        :param image: image for which should be return size
+        :return: height and width for image
+        """
         height, width = image.size
         return height, width
 
-    def copy_all_images_in_and_save_to(self) -> None:
+    def resize_all_images_in_and_save_to(self) -> None:
+        """
+        resize all images and save to new location
+        :return: None
+        """
         paths = self.read_all_in()
         for file, filepath in paths.items():
             image = Image.open(filepath)
@@ -30,15 +48,28 @@ class ImageResizer:
             new_image.save(base_dir)
 
     @staticmethod
-    def get_image_folder_size(file_path:str) -> int:
+    def get_image_folder_size(file_path: str) -> int:
+        """
+        get size of the folder
+        :param file_path: path for folder
+        :return:size of folder in bytes
+        """
         return sum(os.path.getsize(file) for file in os.listdir(file_path) if os.path.isfile(file_path))
 
     def calculate_saved_space_between(self) -> int:
+        """
+        calculate saved space between original folder and processed folde
+        :return: saved space in bytes
+        """
         size_of_original_path = self.get_image_folder_size(self.file_path)
         size_of_processed_path = self.get_image_folder_size(self.destination_path)
         return size_of_original_path-size_of_processed_path
 
     def recalculate_from_bytes_to_mg(self) -> float:
+        """
+        recalculation size from bytes to megabytes
+        :return: size in megabytes
+        """
         size = self.calculate_saved_space_between()
         return round(size/1024, 2)
 
--- day_6_image_resizer/test_day6.py
+++ day_6_image_resizer/test_day6.py
@@ -1,12 +1,42 @@
-from unittest import TestCase
+import os
+from pathlib import Path
+import unittest
 from .day6 import ImageResizer
 
-base_dir = ""
-new_dir = ""
+base_dir = Path(__file__).resolve().parent
+photo_path = os.path.join(base_dir, 'photo')
+destination_path = os.path.join(base_dir, 'smaller')
 
-class TestImageResizer(TestCase):
 
-    def setUpClass(self):
+class TestImageResizer(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.test = ImageResizer(photo_path, destination_path)
         print('Test for TestImageResizer started now..')
-        test_object = ImageResizer(base_dir, new_dir)
 
+    def test_read_all_in(self):
+        self.assertTrue(os.path.isdir(photo_path))
+        self.assertCountEqual(self.test.read_all_in(), {
+            'cat.jpg': os.path.join(base_dir, 'photo', 'cat.jpg'),
+            'cat1.jpg': os.path.join(base_dir, 'photo', 'cat1.jpg')}
+                              )
+
+    def test_get_pixels_size_of(self):
+        pass
+
+    def test_resize_all_images_in_and_save_to(self):
+        pass
+
+    def test_get_image_folder_size(self):
+        pass
+
+    def test_calculate_saved_space_between(self):
+        pass
+
+    def test_recalculate_from_bytes_to_mg(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+    print(base_dir)
