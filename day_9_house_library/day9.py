8459d5cc61ee8c53289e2390ac076d23fda5e821
Joanna Duda <duda.joann@gmail.com>
2021-01-14 22:02:11 +0100

    day 9 typing

--- day_9_house_library/day9.py
+++ day_9_house_library/day9.py
@@ -1,26 +1,21 @@
-"""#HOUSE #LIBRARY
-Napisz program, który importuje katalog z dowolnej biblioteki (np. API Biblioteki Narodowej http://data.bn.org.pl/ -
-przykład użycia: http://data.bn.org.pl/api/bibs.json?author=Andrzej+Sapkowski&amp;kind=ksi%C4%85%C5%BCka).
- Użytkownik może podać autora i program pokaże mu, jakie książki tego autora są w zbiorach biblioteki.
-Następnie daj użytkownikowi możliwość “wypożyczania” i “zwracania” książek - posiadane pozycje są składowane
-w pliku zawierającym pewien identyfikujący zbiór danych, np. tytuł, autor, wydawnictwo, rok wydania
- (możesz też użyć lokalnej bazy danych), w przypadku “wypożyczenia” książki są do niego dodawane,
- a w przypadku “zwracania” usuwane.
-Propozycja rozszerzenia: W prostym przypadku lokalne “wypożyczanie” nie ma wpływu na katalog biblioteki,
- czyli w teorii można wypożyczyć książkę nieskończoną liczbę razy. Zabezpiecz program w taki sposób,
-  aby podczas pobierania danych rozpoznawał też pozycje “wypożyczone” lokalnie i
-  nie pokazywał ich już jako wyniki wyszukiwania."""
-
 import requests
 from day_9_house_library.models import MyBooks, engine
 from sqlalchemy.orm import sessionmaker
 
 
 class HouseLibrary:
-    def __init__(self, author:str)->None:
+    def __init__(self, author: str)->None:
+        """
+        a parameter which will be used to  get data from api
+        :param author:
+        """
         self.author = author
 
     def get_api(self):
+        """
+        get data from api
+        :return:  dictionary with results
+        """
         url = 'http://data.bn.org.pl/api/bibs.json'
         params = {'author': self.author, 'limit':100}
         request = requests.get(url, params = params)
@@ -42,16 +37,19 @@ class HouseLibrary:
 
     @staticmethod
     def get_all_books():
+        """
+        get all borrowed books """
         return MyBooks.query.all()
 
     def borrow(self, book_id: int) -> None:
+        """borrow book, saved to database to table my books"""
         results = self.get_api()
 
         new_book = MyBooks(
-                            author = results[book_id]['author'],
-                            title = results[book_id]['title'],
-                            publisher = results[book_id]['publisher'],
-                            publication_year = results[book_id]['publicationYear']
+                            author=results[book_id]['author'],
+                            title=results[book_id]['title'],
+                            publisher=results[book_id]['publisher'],
+                            publication_year=results[book_id]['publicationYear']
         )
 
         Session = sessionmaker(bind=engine)
@@ -61,7 +59,9 @@ class HouseLibrary:
 
         return self.get_all_books()
 
-    def remove(self, book_id) -> None:
+    def remove(self, book_id: int) -> None:
+        """
+        def remove book from my books, with id """
         book = MyBooks.query.filter(id = book_id).first()
         Session = sessionmaker(bind=engine)
         session = Session()
@@ -71,7 +71,7 @@ class HouseLibrary:
         return self.get_all_books()
 
 
-def main():
+def main()-> None:
     author = input('Please provide author name:')
     results = HouseLibrary(author).get_api()
     for key, value in results.items():
